e158fe4a9ab3ce8c21c76159cd3f197b9d01dcfb
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2025-01-09 19:25:44 -0300

    fix url

--- front/src/utils/axios.js
+++ front/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // http://localhost:5000/api
 const clientAxios = axios.create({
-    baseURL: 'https://coup-server-hls8.onrender.com'
+    baseURL: 'https://coup-server-hls8.onrender.com/api'
 });
 
 export default clientAxios;
